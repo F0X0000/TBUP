023592b991e2615bb91d8753d090b4c98d2dd1e6
Maksymilian Bielecki <maksymilian.bielecki@infotech.edu.pl>
2023-10-29 01:08:06 +0200

    termianl + main update

--- terminal/main.py
+++ terminal/main.py
@@ -1,7 +1,24 @@
 import subprocess
 import os
+import terminal
 import time
-subprocess.Popen(["python3", "server.py"])
+server = subprocess.Popen(["python3", "server.py"])
 username = os.getlogin()
 import xmlrpc.client
+time.sleep(5)
 client = xmlrpc.client.ServerProxy("http://localhost:8000")
+client.changedirectory("/home/" + username)
+
+while True:
+    try:
+        exit = terminal.start()
+        if exit == False:
+            server.kill()
+            server.wait()
+            break
+    except KeyboardInterrupt:
+        pass
+    except EOFError:
+        pass
+    except ValueError:
+        pass
---
+++ terminal/terminal.py
@@ -0,0 +1,47 @@
+import os
+import re
+import xmlrpc.client
+from prompt_toolkit import PromptSession
+ps = PromptSession()
+client = xmlrpc.client.ServerProxy("http://localhost:8000")
+style = ">"
+username = os.getlogin()
+qw = [" ", " "]
+qe = ['', ' ']
+
+def file_exists(file_path):
+    return os.path.exists(file_path)
+def inp(style):
+    i = 0
+    q = ps.prompt(style)
+    if q == qe:
+        q = ' '
+        return q
+    else:
+        q = q.lower()
+        list  =  re.findall(r'"[^"]+"|\S+', q)
+        list = [re.sub('[\'"]', '', token) for token in list]
+        for i in range(50):
+            list.extend(['0'])
+        return list
+
+def openpy(q, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10):
+    path = os.path.dirname(os.getcwd())
+    script_path = f"python3 /{path}/{q}.py {q1} {q2} {q3} {q4} {q5} {q6} {q7} {q8} {q9} {q10}"
+    if file_exists(f"{path}/{q}.py"):
+        os.system(script_path)
+    else:
+        return True
+def start():
+    q = inp(style)
+    if q[0] == '0':
+        return True
+    elif q[0] == 'exit':
+        return False
+    else:
+        end = openpy(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7], q[8], q[9], q[10])
+        if end == True:
+            print(f"'{q[0]}' is not recognized as an internal or external command,operable program or file.")
+            return True
+        return True
+    return True
